6718296f8aa59e8c17de4a8b6d7a451a45129444
krishnasori <krishnasori22@gmail.com>
2026-08-08 23:44:25 +0530

    Implement totalFruit function in Solution class

---
+++ DSA revision/array revision/fruits in basket.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+         int n = fruits.size();
+        int left = 0;
+        int ans = 0;
+        unordered_map<int, int> freq;
+        for (int right = 0; right < n; right++) {
+            freq[fruits[right]]++;
+            while (freq.size() > 2) {
+                freq[fruits[left]]--;
+                if (freq[fruits[left]] == 0) {
+                    freq.erase(fruits[left]);
+                }
+                left++;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
